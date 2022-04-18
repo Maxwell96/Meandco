2141a41b8b7fc4ad3a79b66eec0bb8e9d06086f7
Maxwell Amoako Agyapong <agyapongmaxwell26@gmail.com>
2022-04-18 09:40:11 -0400

    Added styling and content to the homepage

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 # Create your views here.
-from django.http import HttpResponse
+
+# We are using functional based views 
 
 def index(request):
-    return HttpResponse('<h1>The Meandco Homepage</h1>')
-
+    # render is a special django function that creates a shortcut for communicating with the web browser
+    # It returns the appropriately formatted response without you having to code the intermediate steps
+    # We are returning the original request object from the browser and the name of our site template
+    return render(request, 'base.html') 
